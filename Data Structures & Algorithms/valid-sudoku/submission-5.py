615c91d5f3405d81814cec88f10f8068a7c44fef
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2026-04-10 00:37:01 -0400

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # store rows cols and squares as a set
+        # if any of 3 conditions - return false exit immediately
+        # // 3 ? --- [r // 3 , c // 3]
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                # if empty cell continue
+                if board[r][c] == ".":
+                    continue
+                if(board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                # add cells val so future cells can check if its a dup
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+                
+        return True 
